4e8c56b95169a201e4dc8f17c9a35b65d60440da
Beni Mahat <benimahat6565@gmail.com>
2020-12-24 21:40:52 -0700

    login page styled

--- amazon/src/components/login/Login.js
+++ amazon/src/components/login/Login.js
@@ -5,11 +5,30 @@ import "./Login.css"
 
 function Login() {
     return (
-        <Link to="/">
         <div className="login">
+        <Link to="/">
             <img className="login__logo" src="http://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt=""/>
-        </div>
         </Link>
+
+        <div className="login__container">
+            <h1>Sing-in</h1>
+            <form>
+                <h5>E-mail</h5>
+                <input type="text"></input>
+
+                <h5>Password</h5>
+                <input type="password"></input>
+                
+                <button className="login__signInButton">Sign-In</button>
+            </form>
+
+            <p>
+                By Siging-in you agree to enjoy a demonstration of the AMAZON CLONE CHALLENGE done by Beni Mahat. . Please explore and enjoy all of the features that we have including payment through paypal. This would be a great template for any E-comerce based website. Credits to Clever Programers for the toutorial. 
+            </p>
+
+            <button className="login__registerButton">Create your Amazon Account</button>
+        </div>
+        </div>
     )
 }
 
